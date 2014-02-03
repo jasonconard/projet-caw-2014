d3ef92b30ea77a133dbe53ace1db0a8124dfb3d6
Jason Conard <conard.jason@gmail.com>
2014-02-03 18:44:22 +0100

    Correction mais un bogue, chemin eclipse

--- CharacterConvertWebApp/src/main/java/Controller/ConnecteurApp.java
+++ CharacterConvertWebApp/src/main/java/Controller/ConnecteurApp.java
@@ -1,8 +1,10 @@
 package Controller;
 
 import java.io.IOException;
+import java.util.Enumeration;
 
 import javax.servlet.ServletException;
+import javax.servlet.ServletInputStream;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -11,12 +13,12 @@ import Model.JsonToFile;
 
 public class ConnecteurApp extends HttpServlet{
 
-	
+	private static final long serialVersionUID = -7758388960799525831L;
+
 	public void doPost(HttpServletRequest req, HttpServletResponse res)
-	throws ServletException {
-		String chaine = req.getParameter("chaine-caractere");
-		
-		
+	throws ServletException, IOException {
+		String chaine = req.getParameter("chaine");
+		//System.out.println("Chaine = "+chaine);
 		JsonToFile jtf;
 		try {
 			jtf = new JsonToFile(chaine);
--- CharacterConvertWebApp/src/main/java/Model/JsonToFile.java
+++ CharacterConvertWebApp/src/main/java/Model/JsonToFile.java
@@ -45,8 +45,10 @@ public class JsonToFile {
 	}
 	
 	private void creationWriter() throws IOException{
+		
 		try{
-			bw = new BufferedWriter(new FileWriter("json_file.json"));
+			bw = new BufferedWriter(new FileWriter(fichier));
+			System.out.println("ok! ");
 		}catch(IOException ieo){
 			throw new IOException("Erreur creation fichier "+ fichier);
 		}
