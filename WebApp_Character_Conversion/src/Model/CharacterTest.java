03f2412ba5b94e3e14ce4c7905ddfa13a1affcbc
Jason Conard <conard.jason@gmail.com>
2014-02-03 10:03:57 +0100

    MAJ structure avec partie serveur

--- WebApp_Character_Conversion/src/Model/CharacterTest.java
+++ WebApp_Character_Conversion/src/Model/CharacterTest.java
@@ -2,12 +2,20 @@ package Model;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.apache.commons.lang3.*;
 import static org.junit.Assert.*;
  
 public class CharacterTest {
 
     @Test
     public void testToJson() throws Exception {
+    	Character c = new Character('é');
+    	c.init();
+    	String expectedResult = "{\"é\", \"233\",\"e9\",\"&eacute;\"}";
+    	System.out.println(expectedResult);
+    	String currentResult = c.toJson(); 
+    	System.out.println(currentResult);
+    	assertEquals(expectedResult,currentResult);
     	
     }
 }
