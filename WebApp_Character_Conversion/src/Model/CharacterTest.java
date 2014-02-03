6e8e55e8a106987cb2e5e178089c6566f5ddb46d
Jason Conard <conard.jason@gmail.com>
2014-02-03 09:44:28 +0100

    MAJ structure avec partie serveur

---
+++ WebApp_Character_Conversion/src/Model/CharacterTest.java
@@ -0,0 +1,13 @@
+package Model;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+ 
+public class CharacterTest {
+
+    @Test
+    public void testToJson() throws Exception {
+    	
+    }
+}
